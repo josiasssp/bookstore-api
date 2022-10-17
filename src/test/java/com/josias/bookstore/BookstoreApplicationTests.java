8a909baf2b5de20d3bf273f9678b810e4fe5c2fd
josiasssp <josiasfernandessp@gmail.com>
2022-10-17 11:54:24 -0300

    commit inicial

---
+++ src/test/java/com/josias/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josias.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
